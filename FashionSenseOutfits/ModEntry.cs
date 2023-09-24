1e9a4d62875c7655f0ffcc1ce7053bf701663f5f
Nihilistzsche <nihilistzsche@gmail.com>
2023-09-23 18:53:22 -0700

    Cleanup
    File Headers and ReSharper cleanup

--- FashionSenseOutfits/ModEntry.cs
+++ FashionSenseOutfits/ModEntry.cs
@@ -1,23 +1,38 @@
-﻿using FashionSense.Framework.Interfaces.API;
-
-using FashionSenseOutfits.Models;
-
-using StardewModdingAPI;
-using StardewModdingAPI.Events;
-
-using StardewValley;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// Copyright (C) 2023 Nihilistzsche
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace FashionSenseOutfits
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using FashionSense.Framework.Interfaces.API;
+
+    using Models;
+
+    using StardewModdingAPI;
+    using StardewModdingAPI.Events;
+
+    using StardewValley;
+
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
-        internal static IApi fsApi;
-        internal static Dictionary<string, ModData> data;
+        private static IApi _fsApi;
+        private static Dictionary<string, ModData> _data;
 
         public override void Entry(IModHelper helper)
         {
@@ -29,82 +44,82 @@ namespace FashionSenseOutfits
             helper.Events.Content.AssetReady += OnAssetReady;
         }
 
-        private (bool Valid, string CorrectedID) IsValid(string outfitID)
+        private static (bool Valid, string CorrectedID) IsValid(string outfitId)
         {
-            if (outfitID != null && outfitID != "")
+            if (string.IsNullOrEmpty(outfitId))
             {
-                var outfitIDs = fsApi.GetOutfitIds().Value;
-                var correctedID = outfitIDs.Where(x => x.Equals(outfitID, StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
-                return (correctedID != null, correctedID);
+                return (false, null);
             }
-            return (false, null);
+
+            var outfitIDs = _fsApi.GetOutfitIds().Value;
+            var correctedId = outfitIDs.FirstOrDefault(x => x.Equals(outfitId, StringComparison.OrdinalIgnoreCase));
+            return (correctedId != null, correctedId);
         }
 
-        private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
         {
-            fsApi = Helper.ModRegistry.GetApi<IApi>("PeacefulEnd.FashionSense");
+            _fsApi = Helper.ModRegistry.GetApi<IApi>("PeacefulEnd.FashionSense");
         }
 
         private void LoadData(dynamic e)
         {
+            // ReSharper disable once InvertIf
             if (e.GetType().GetProperty("IsLocalPlayer") == null || e.IsLocalPlayer)
             {
                 Helper.GameContent.InvalidateCache("nihilistzsche.FashionSenseOutfits/Outfits");
-                data = Game1.content.Load<Dictionary<string, ModData>>("nihilistzsche.FashionSenseOutfits/Outfits");
+                _data = Game1.content.Load<Dictionary<string, ModData>>("nihilistzsche.FashionSenseOutfits/Outfits");
             }
         }
 
-        private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e) => LoadData(e);
+        private void OnSaveLoaded(object sender, SaveLoadedEventArgs e) => LoadData(e);
 
-        private void OnDayStarted(object? sender, DayStartedEventArgs e) => LoadData(e);
+        private void OnDayStarted(object sender, DayStartedEventArgs e) => LoadData(e);
 
-        private void OnWarped(object? sender, WarpedEventArgs e) => LoadData(e);
+        private void OnWarped(object sender, WarpedEventArgs e) => LoadData(e);
 
-        private void OnAssetRequested(object? sender, AssetRequestedEventArgs e)
+        // ReSharper disable once MemberCanBeMadeStatic.Local
+        private void OnAssetRequested(object sender, AssetRequestedEventArgs e)
         {
             if (e.Name.IsEquivalentTo("nihilistzsche.FashionSenseOutfits/Outfits"))
             {
-                e.LoadFrom(() =>
-                {
-                    return new Dictionary<string, ModData>
-                    {
-                        ["CurrentOutfit"] = new ModData { OutfitID = "" }
-                    };
-                }, AssetLoadPriority.Medium);
+                e.LoadFrom(
+                    () => new Dictionary<string, ModData>
+                        {
+                            ["CurrentOutfit"] = new () { OutfitID = string.Empty },
+                        },
+                    AssetLoadPriority.Medium);
             }
         }
 
-        private void OnAssetReady(object? sender, AssetReadyEventArgs e)
+        private void OnAssetReady(object sender, AssetReadyEventArgs e)
         {
+            // ReSharper disable once InvertIf
             if (e.Name.IsEquivalentTo("nihilistzsche.FashionSenseOutfits/Outfits"))
             {
-                data = Game1.content.Load<Dictionary<string, ModData>>("nihilistzsche.FashionSenseOutfits/Outfits");
+                _data = Game1.content.Load<Dictionary<string, ModData>>("nihilistzsche.FashionSenseOutfits/Outfits");
                 UpdateOutfit();
             }
         }
 
         private void UpdateOutfit()
         {
-            var currentOutfitID = data["CurrentOutfit"].OutfitID;
-            var CurrentOutfitPair = fsApi.GetCurrentOutfitId();
-            var (Valid, CorrectedID) = IsValid(currentOutfitID);
-            if (!Valid)
+            var currentOutfitId = _data["CurrentOutfit"].OutfitID;
+            var currentOutfitPair = _fsApi.GetCurrentOutfitId();
+            var (valid, correctedId) = IsValid(currentOutfitId);
+            if (!valid)
             {
-                Monitor.Log($"Given outfit {currentOutfitID} is invalid.");
+                Monitor.Log($"Given outfit with ID {currentOutfitId} is invalid.");
                 return;
             }
-            else if (currentOutfitID != CorrectedID)
+
+            if (currentOutfitPair.Key && correctedId == currentOutfitPair.Value)
             {
-                currentOutfitID = CorrectedID;
-            }
-            if (CurrentOutfitPair.Key && currentOutfitID == CurrentOutfitPair.Value)
-            {
-                Monitor.Log($"Skipping because outfit ({currentOutfitID}) is already equipped.");
+                Monitor.Log($"Skipping because the outfit with ID {correctedId} is already equipped.");
             }
             else
             {
-                Monitor.Log($"Applying outfit {currentOutfitID} via Fashion Sense API...");
-                fsApi.SetCurrentOutfitId(currentOutfitID, ModManifest);
+                Monitor.Log($"Applying outfit with ID {correctedId} via Fashion Sense API...");
+                _fsApi.SetCurrentOutfitId(correctedId, ModManifest);
             }
         }
     }
